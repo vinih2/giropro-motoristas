f7e7ea9d1e3285ed8986c0df70052f3f343014c8
vinih2 <vinihlins7@gmail.com>
2025-11-14 08:47:10 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: "class", // ✅ Linha importante para o Dark Mode funcionar
+  darkMode: "class", // ✅ ESSENCIAL PARA O DARK MODE FUNCIONAR
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
